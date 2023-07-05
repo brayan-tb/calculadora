5502faae164868f31b0d7d0a81e2a7ef7945cd91
brayan-tb <83845633+brayan-tb@users.noreply.github.com>
2023-07-05 16:06:09 -0300

    fixing key press

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-
 function calculate() {
     try {
-        var result = eval(document.getElementById('result').value);
+        let result = eval(document.getElementById('result').value);
         document.getElementById('result').value = result;
     } catch (error) {
         document.getElementById('result').value = 'Erro';
@@ -16,13 +15,18 @@ function appendValue(value) {
     document.getElementById('result').value += value;
 }
 
+function removeValue() {
+    let input = document.getElementById('result');
+    if(input.value.length > 0) input.value = input.value.slice(0, -1)
+}
+
 window.onload = function() {
     document.addEventListener("keydown", function (event){
         if (event.key >= 0 && event.key <= 9) { appendValue(event.key); }
         else if(event.key == "Escape"){clearValue();}
-        else if(event.key == "." || event.key == "+" || event.key == "-" || event.key == "/" || event.key == "*"){
-            appendValue(event.key);
-        }
         else if(event.key == "Enter"){calculate();}
+        else if(event.key == "." || event.key == "+" || event.key == "-" || event.key == "/" || event.key == "*"){appendValue(event.key);}
+        else if (event.key == "Backspace"){removeValue()}
+        event.preventDefault(); 
     })
 }
